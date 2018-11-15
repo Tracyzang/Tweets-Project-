03355f3946b070e78cdd5efbbd81f0fee4b289fb
MacUser01 <macuser01@MacUser01matoMacBook-Air.local>
2018-11-15 11:03:09 -0800

    update

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import authedUser from "./authedUser";
+import authedUser from "./authedUsers";
 import tweets from "./tweets";
 import users from "./users";
 
